c33d8d77c8eb3c41675de03483a374134d32bf79
jknnth03 <kenneth040396@gmail.com>
2024-10-28 13:22:41 +0800

    My message

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './page/login'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
+import Landing from './components/landing'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Login/>
+      <BrowserRouter>
+         <Routes>
+      <Route path='/Login' element={<Login />}/>    
+      <Route path='/' element={<Landing />}/>
+    </Routes>
+      </BrowserRouter>
   )
 }
 
